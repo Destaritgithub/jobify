91c59a64e5fe0e5695adfa085f5dea8164e99a03
Destaritgithub <alexdesse@gmail.com>
2023-05-30 05:36:54 -0700

    commited

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from 'express';
 const app = express();
+ import dotenv from 'dotenv';
+dotenv.config()
+// middleware
+import notFoundMiddleware from './middleware/notfound.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
 
 app.get('/', (req, res) => {
+  throw new Error('error');
   res.send('Welcome!');
 });
+// middleware
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
